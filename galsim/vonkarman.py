782a3a2b2999884069714798c0b7f4748330e534
Josh Meyers <jmeyers314@gmail.com>
2017-11-07 16:19:10 -0800

    Add vK documentation

--- galsim/vonkarman.py
+++ galsim/vonkarman.py
@@ -30,12 +30,58 @@ from .gsobject import GSObject
 
 
 class VonKarman(GSObject):
+    """Class describing a von Karman surface brightness profile, which represents a long exposure
+    atmospheric PSF.  The difference between the von Karman profile and the related Kolmogorov
+    profile is that the von Karman profile includes a parameter for the outer scale of atmospheric
+    turbulence, which is a physical scale beyond which fluctuations in the refractive index stop
+    growing, typically between 10 and 100 meters.  Quantitatively, the von Karman phase fluctuation
+    power spectrum is proportional to
+
+        (f^2 + L0^-2)^(-11/6)
+
+    where f is a spatial frequency and L0 is the outer scale in meters.  The Kolmogorov power
+    spectrum proportional to f^(-11/3) is recovered as L0 -> infinity,
+
+    For more information, we recommend the following references:
+
+        Martinez et al.  2010  A&A  vol. 516
+        Conan  2008  JOSA  vol. 25
+
+    Notes
+    -----
+
+    If one blindly follows the math for converting the von Karman power spectrum into a PSF, one
+    finds that the PSF contains a delta-function at the origin with fractional flux of
+
+        exp(-0.5*0.172*(r0/L0)^(-5/3))
+
+    In almost all cases of interest this evaluates to something tiny, often on the order of 10^-100
+    or smaller.  By default, GalSim will ignore this delta function entirely.  If for some reason
+    you want to keep the delta function, though, then you can pass the doDelta=True argument to the
+    VonKarman initializer.
+
+    @param lam              Wavelength in nanometers
+    @param r0               Fried parameter in meters.
+    @param L0               Outer scale in meters.  [default: np.inf]
+    @param flux             The flux (in photons/cm^2/s) of the profile. [default: 1]
+    @param scale_unit       Units assumed when drawing this profile or evaluating xValue, kValue,
+                            etc.  Should be a galsim.AngleUnit or a string that can be used to
+                            construct one (e.g., 'arcsec', 'radians', etc.).
+                            [default: galsim.arcsec]
+    @param doDelta          Include delta-function at origin? (not recommended; see above).
+                            [default: False]
+    @param gsparams         An optional GSParams argument.  See the docstring for GSParams for
+                            details. [default: None]
+    """
+
     def __init__(self, lam, r0, L0=np.inf, flux=1, scale_unit=galsim.arcsec,
                  doDelta=False, gsparams=None):
         # We lose stability if L0 gets too large.  This should be close enough to infinity for
         # all practical purposes though.
         if L0 > 1e10:
             L0 = 1e10
+        if isinstance(scale_unit, str):
+            scale_unit = galsim.angle.get_angle_unit(scale_unit)
         # Need _scale_unit for repr roundtriping.
         self._scale_unit = scale_unit
         scale = scale_unit/galsim.arcsec
